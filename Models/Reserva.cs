cfbbc0488512c90e81cb8b811431176e7c963d8b
Ayslan Câmara <ayslan.trabalho@gmail.com>
2022-12-30 17:04:11 -0300

    desafio concluido

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -3,6 +3,11 @@ namespace DesafioProjetoHospedagem.Models
     public class Reserva
     {
         public List<Pessoa> Hospedes { get; set; }
+        public Reserva(Suite suite) 
+        {
+            this.Suite = suite;
+   
+        }
         public Suite Suite { get; set; }
         public int DiasReservados { get; set; }
 
@@ -17,14 +22,17 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
-            {
-                Hospedes = hospedes;
-            }
+
+                if (hospedes.Count() <= Suite.Capacidade)
+                {
+                    Hospedes = hospedes;
+                }        
+
             else
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
                 // *IMPLEMENTE AQUI*
+                throw new Exception("O número de hóspedes é maior do que a capaciodade da suíte");
             }
         }
 
@@ -35,9 +43,7 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            return Hospedes.Count();
         }
 
         public decimal CalcularValorDiaria()
@@ -45,13 +51,14 @@ namespace DesafioProjetoHospedagem.Models
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
             // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            decimal valor = DiasReservados*Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados > 10)
             {
-                valor = 0;
+                decimal desconto = 0.9M;
+                valor = valor  * desconto;
             }
 
             return valor;
